f3751ca56c4809decc1ec3845e5c301a13292773
Kristof Zerbe <kristof.zerbe@gmail.com>
2023-06-09 15:17:48 +0200

    Make popup reusable

--- downupPopup/downupPopup.js
+++ downupPopup/downupPopup.js
@@ -7,10 +7,27 @@
 (function ($) {
     $.fn.downupPopup = function (options) {
         const $this = $(this);
+
+        // Option Handling
+        if (typeof options === "object" || !options) { 
+            var settings = $.extend({
+                duration: "300",
+                animation: "ease",
+                background: true,
+                radiusLeft: "10px",
+                radiusRight: "10px",
+                distance: 20,
+                headerText: "",
+                width: "100%",
+                contentScroll: false
+            }, options);
+        }
+
+        // Preparation
         if (!$this.hasClass("downupPopup")) {
             $this.addClass("downupPopup")
                 .addClass("no-act")
-                .prepend('<div class="downupPopup-header"><svg class="downupPopup-kapat" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></div>');
+                .prepend('<div class="downupPopup-header"><span></span><svg class="downupPopup-kapat" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></div>');
 
             $this.find(".downupPopup-kapat").click(function () {
                 $this.addClass("no-act");
@@ -27,34 +44,7 @@
                 $("body").css("overflow", "inherit");
             });
 
-            if (typeof options === "object" || !options) { // obje olarak geliyorsa $().downupPopup({deneme:"deneme"}); veya boşsa
-                // Default ayar
-                var settings = $.extend({
-                    duration: "300",
-                    animation: "ease",
-                    background: true,
-                    radiusLeft: "10px",
-                    radiusRight: "10px",
-                    distance: 20,
-                    headerText: "",
-                    width: "100%",
-                    contentScroll: false
-                }, options);
-                $this.attr("distance", settings.distance)
-                    .find(".downupPopup-header")
-                    .prepend('<span>' + settings.headerText + '</span>');
-
-                setTimeout(() => {
-                    $this.css('transition', 'transform ' + settings.duration + 'ms ' + settings.animation + '');
-                    $this.css('border-radius', '' + settings.radiusLeft + ' ' + settings.radiusRight + ' 0px 0px');
-                    $this.css('width', '' + settings.width + '');
-                    const hgt = (((100 - settings.distance) - 10) + 3);
-                    if (settings.contentScroll) {
-                        $this.find(".downupPopup-content").css('height', '' + hgt + 'vh');
-                        $this.find(".downupPopup-content").css('overflow-y', 'scroll');
-                    }
-                }, 100);
-
+            if (typeof options === "object" || !options) { 
                 if (settings.background == true) {
                     if (!$("#dark-back")[0])
                         $("body").append("<div id='dark-back'></div>");
@@ -63,7 +53,26 @@
                 }
             }
         }
-        if (typeof options !== "object") {// obje değil de düz yazı geliyorsa $().downupPopup("open");
+        
+        // Initialization
+        if (typeof options === "object" || !options) {
+            setTimeout(() => {
+                $this.attr("distance", settings.distance)
+                    .find(".downupPopup-header span").text(settings.headerText);
+                    
+                $this.css('transition', 'transform ' + settings.duration + 'ms ' + settings.animation + '');
+                $this.css('border-radius', '' + settings.radiusLeft + ' ' + settings.radiusRight + ' 0px 0px');
+                $this.css('width', '' + settings.width + '');
+                const hgt = (((100 - settings.distance) - 10) + 3);
+                if (settings.contentScroll) {
+                    $this.find(".downupPopup-content").css('height', '' + hgt + 'vh');
+                    $this.find(".downupPopup-content").css('overflow-y', 'scroll');
+                }
+            }, 100);
+        }
+
+        // Calling
+        if (typeof options !== "object") {
             if (options === "open") {
                 $(".downupPopup").addClass("no-act");
                 if ($this.attr("bg") == 1)
